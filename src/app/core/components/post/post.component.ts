2d8a84fbb7bc3453abd5868b65c6826d6c60767f
NikitaGlukhi <nikita.glukhi@valor-software.com>
2023-10-03 09:02:20 +0300

    added call/apply methods

--- src/app/core/components/post/post.component.ts
+++ src/app/core/components/post/post.component.ts
@@ -1,6 +1,17 @@
-import { Input, Component, } from '@angular/core';
+import { Input, AfterViewInit, ViewChild, Component } from '@angular/core';
 
 import { IPost } from '../../models';
+import { LikesComponent } from '../likes/likes.component';
+
+function totalPostLikes(checkDate: number, username: string): void {
+  // @ts-ignore
+  prompt(`This post was liked ${this.likes?.length} time(-s). Checked ${new Date(checkDate).toUTCString()} by ${username}.`);
+}
+
+function totalPostLikesBind(): void {
+  // @ts-ignore
+  prompt(`This post was liked ${this.likes?.length} time(-s).`);
+}
 
 @Component({
   selector: 'post-core',
@@ -8,9 +19,17 @@ import { IPost } from '../../models';
   styleUrls: ['./post.component.scss'],
 })
 export class PostComponent {
+  @ViewChild('likes', { read: LikesComponent }) likesComponent?: LikesComponent;
+
   @Input() post?: IPost;
   @Input() currentUserId?: string;
   @Input() userName?: string;
+
+  countTotalLikes(): void {
+    totalPostLikes.apply(this.likesComponent, [Date.now(), this.userName || 'N/A']); // will get expected result
+    totalPostLikes.call(this.likesComponent, Date.now(), this.userName || 'N/A'); // will get expected result
+    totalPostLikes(Date.now(), this.userName || 'N/A'); // will get an error
+  }
 
   likedByUser = (): boolean => {
     const like = this.post?.likes.find(like => like.likedBy === this.currentUserId);
