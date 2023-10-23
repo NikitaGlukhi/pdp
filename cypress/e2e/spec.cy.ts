0594097bb81633e98a6698ff1742b77d9f58b9c9
NikitaGlukhi <nikita.glukhi@valor-software.com>
2023-10-23 18:20:30 +0300

    implemented e2e auth page testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,16 @@
-describe('template spec', () => {
+describe('Test login page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:4300')
-  })
+    cy.visit('http://localhost:4300');
+
+    cy.url().should('include', '/auth');
+
+    cy.get('.enter-login').type('Nikita_Glukhi');
+    cy.get('.enter-login').should('have.value', 'Nikita_Glukhi');
+
+    cy.get('.enter-password').type('test12345@');
+    cy.get('.enter-password').should('have.value', 'test12345@');
+
+    cy.get('.btn-info').click();
+    cy.url().should('include', '/');
+  });
 })
